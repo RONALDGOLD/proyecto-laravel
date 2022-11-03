afccd2bfaf9c05948f497ca1cfee3f9ecb82ec34
ronald gold <ronaldgold.fernandez@gmail.com>
2022-11-02 20:19:31 -0600

    proyectito

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Llamamos a todos los controladores que usaremos
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\ProductoController;    
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PedidosController;
+
+// Creando una ruta para listar clientes
+Route::get('/clientes', 
+  [CustomerController::class, 'listar']
+);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/usuarios', function() {
+    return "Aqui van los usuariosxxxxxxx";
+});
+
+Route::get('/usuarios2', 
+  [UsuariosController::class, 'listar']
+);
+
+Route::get('/productos', 
+  [ProductoController::class, 'listar']
+);
+
+Route::get('/pedidos',
+  [PedidosController::class, 'listar']
+);
